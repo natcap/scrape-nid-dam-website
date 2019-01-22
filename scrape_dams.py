a7368aa042181c9e8825b63bc4df865656d4d646
Rich Sharp <richpsharp@gmail.com>
2019-01-22 15:23:14 -0800

    going state by state

--- scrape_dams.py
+++ scrape_dams.py
@@ -1,54 +1,83 @@
 """Scrape dam inventory."""
+import os
 import time
 
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
+
+PRIMARY_URL = "http://nid.usace.army.mil/cm_apex/f?p=838:4:0::NO"
+TABLE_PATH = 'table.csv'
+
 
 def main():
+    if os.path.exists(TABLE_PATH):
+        os.remove(TABLE_PATH)
 
-    my_url = "http://nid.usace.army.mil/cm_apex/f?p=838:4:0::NO"
-    driver = webdriver.PhantomJS()
-    driver.get(my_url)
+    wrote_header = False
+    for state_code in (
+            'AZ', 'AL', 'AK', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI',
+            'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI',
+            'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC',
+            'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
+            'VT', 'VA', 'WA', 'WV', 'WI', 'WY'):
 
-    driver.save_screenshot("screenshot.png")
-    el = driver.find_element_by_id('P12_ORGANIZATION')
-    for option in el.find_elements_by_tag_name('option'):
-        print(option.text)
-        if option.text == 'Academia':
-            option.click()  # select() in earlier versions of webdriver
-            print('clicked academia')
-            break
+        driver = webdriver.PhantomJS()
+        driver.get(PRIMARY_URL)
 
-    driver.save_screenshot("screenshot2.png")
-    link = driver.find_element_by_link_text('NID Interactive Report')
-    link.click()
-    driver.save_screenshot("screenshot3.png")
+        driver.save_screenshot(f"screenshot_{state_code}.png")
+        el = driver.find_element_by_id('P12_ORGANIZATION')
+        for option in el.find_elements_by_tag_name('option'):
+            if option.text == 'Academia':
+                option.click()  # select() in earlier versions of webdriver
+                print('clicked academia')
+                break
 
-    search_link = driver.find_element_by_id('apexir_SEARCHDROPROOT')
-    search_link.click()
-    time.sleep(5)
-    driver.save_screenshot("screenshot4.png")
+        driver.save_screenshot(f"screenshot2_{state_code}.png")
 
-    el = driver.find_element_by_id('apexir_columnsearch')
-    el.find_element_by_id('STATE').click()
-    time.sleep(5)
-    driver.save_screenshot("screenshot5.png")
+        print('click NID Interactive Report')
+        link = driver.find_element_by_link_text('NID Interactive Report')
+        link.click()
+        driver.save_screenshot(f"screenshot3_{state_code}.png")
 
-    input_box = driver.find_element_by_id('apexir_SEARCH')
-    input_box.clear()
-    input_box.send_keys('AZ')
-    driver.save_screenshot("screenshot6.png")
-    driver.execute_script("javascript:gReport.search('SEARCH',100000)")
-    time.sleep(5)
-    driver.save_screenshot("screenshot7.png")
+        print('click search')
+        search_link = driver.find_element_by_id('apexir_SEARCHDROPROOT')
+        search_link.click()
+        time.sleep(1)
+        driver.save_screenshot(f"screenshot4_{state_code}.png")
 
+        el = driver.find_element_by_id('apexir_columnsearch')
+        el.find_element_by_id('STATE').click()
+        print('click STATE')
+        time.sleep(1)
+        driver.save_screenshot(f"screenshot5_{state_code}.png")
 
-    #table = driver.find_element_by_class_name('apexir_WORKSHEET_DATA')
-    #print(table)
+        print(f'type {state_code}')
+        input_box = driver.find_element_by_id('apexir_SEARCH')
+        input_box.clear()
+        input_box.send_keys(state_code)
+        driver.save_screenshot(f"screenshot6_{state_code}.png")
+        print('search')
+        driver.execute_script("javascript:gReport.search('SEARCH',100000)")
+        print('executed 100000 search')
+        time.sleep(5)
+        driver.save_screenshot(f"screenshot7_{state_code}.png")
+        print("loading table via xpath")
+        table_element = driver.find_element_by_xpath(
+            "//table[@class='apexir_WORKSHEET_DATA']")
+        print("loaded table doc from a string")
+        with open(TABLE_PATH, 'a') as table_file:
+            for row_index, table_row in enumerate(
+                    table_element.find_elements_by_xpath(".//tr")):
+                if not wrote_header:
+                    for col in table_row.find_elements_by_xpath(".//th"):
+                        table_file.write(f'{col.text},')
+                    table_file.write('\n')
+                    wrote_header = True
 
-    #apex.submit(&#x27;P12_ORGANIZATION&#x27;)
-    #p_element = driver.find_element_by_id(id_='intro-text')
-    #print(p_element.text)
+                if row_index % 100 == 0:
+                    print(f'{state_code}, {row_index}')
+                for col in table_row.find_elements_by_xpath(".//td"):
+                    table_file.write(f'{col.text},')
+                table_file.write('\n')
 
 
 if __name__ == '__main__':
