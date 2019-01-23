80c78a322c29cd1559c36189827f7a5be8258919
Rich Sharp <richpsharp@gmail.com>
2019-01-22 18:42:36 -0800

    code to clear old search rather than start new page

--- scrape_dams.py
+++ scrape_dams.py
@@ -9,68 +9,85 @@ import time
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
 
 PRIMARY_URL = "http://nid.usace.army.mil/cm_apex/f?p=838:4:0::NO"
 TABLE_PATH = 'table.csv'
-
+N_WORKERS = 8
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-def search_state(state_code, work_queue):
+def search_state(work_queue, write_queue):
     """Search for state `state_code` and dumps result to `work_queue`."""
     firefox_options = Options()
     firefox_options.headless = True
     driver = webdriver.Firefox(options=firefox_options)
     driver.implicitly_wait(10)
     driver.get(PRIMARY_URL)
-
     el = driver.find_element_by_id('P12_ORGANIZATION')
     for option in el.find_elements_by_tag_name('option'):
         if option.text == 'Academia':
             option.click()  # select() in earlier versions of webdriver
             LOGGER.info('clicked academia')
             break
-
     LOGGER.info('click NID Interactive Report')
     link = driver.find_element_by_link_text('NID Interactive Report')
     link.click()
-    LOGGER.info('click search')
-    time.sleep(0.1)
-    driver.save_screenshot(f"screenshot_search_{state_code}.png")
-    search_link = driver.find_element_by_id('apexir_SEARCHDROPROOT')
-    search_link.click()
-    time.sleep(0.1)
-    driver.save_screenshot(f"screenshot_searchdropdown_{state_code}.png")
-    el = driver.find_element_by_id('apexir_columnsearch')
-    el.find_element_by_id('STATE').click()
-    LOGGER.info('click STATE')
-    time.sleep(0.1)
-    driver.save_screenshot(f"screenshot_searchstate_{state_code}.png")
-    LOGGER.info(f'type {state_code}')
-    input_box = driver.find_element_by_id('apexir_SEARCH')
-    input_box.clear()
-    input_box.send_keys(state_code)
-    driver.save_screenshot(f"screenshot_searchscode_{state_code}.png")
-    time.sleep(0.1)
-    driver.execute_script("javascript:gReport.search('SEARCH',100)")
-    time.sleep(5.0)
-    driver.save_screenshot(f"screenshot_searcch100_{state_code}.png")
-    LOGGER.info('executed 100000 search')
+
     while True:
-        try:
-            table_element = driver.find_element_by_xpath(
-                "//table[@class='apexir_WORKSHEET_DATA']")
-            bs_table_rows = BeautifulSoup(table_element.get_attribute(
-                'outerHTML'), 'html.parser').select(
-                    'table.apexir_WORKSHEET_DATA tr')
+        state_code = work_queue.get()
+        if state_code == 'STOP':
             break
-        except StaleElementReferenceException:
-            LOGGER.info('stale element, trying again')
-            time.sleep(0.5)
-    driver.save_screenshot(f"screenshot_{state_code}.png")
-    work_queue.put((bs_table_rows, state_code))
+
+        try:
+            delete_filter_element = driver.find_element_by_xpath(
+                "//a[contains(@onclick, 'gReport.column.filter_delete')]")
+            LOGGER.info(delete_filter_element)
+            if delete_filter_element:
+                LOGGER.info("deleting old filter")
+                driver.execute_script("javascript:gReport.search('SEARCH',1)")
+                time.sleep(0.5)
+                delete_filter_element = driver.find_element_by_xpath(
+                    "//a[contains(@onclick, 'gReport.column.filter_delete')]//img")
+                delete_filter_element.click()
+                time.sleep(0.5)
+                driver.save_screenshot(f"cleared_filter_{state_code}.png")
+        except NoSuchElementException:
+            LOGGER.exception("no such element")
+            pass
+
+        LOGGER.info('click search')
+        time.sleep(0.1)
+        search_link = driver.find_element_by_id('apexir_SEARCHDROPROOT')
+        search_link.click()
+        time.sleep(0.1)
+        el = driver.find_element_by_id('apexir_columnsearch')
+        el.find_element_by_id('STATE').click()
+        LOGGER.info('click STATE')
+        time.sleep(0.1)
+        LOGGER.info(f'type {state_code}')
+        input_box = driver.find_element_by_id('apexir_SEARCH')
+        input_box.clear()
+        input_box.send_keys(state_code)
+        time.sleep(0.1)
+        driver.execute_script("javascript:gReport.search('SEARCH',100)")
+        time.sleep(5.0)
+        LOGGER.info('executed 100000 search')
+        driver.save_screenshot(f"post_search_{state_code}.png")
+        while True:
+            try:
+                table_element = driver.find_element_by_xpath(
+                    "//table[@class='apexir_WORKSHEET_DATA']")
+                bs_table_rows = BeautifulSoup(table_element.get_attribute(
+                    'outerHTML'), 'html.parser').select(
+                        'table.apexir_WORKSHEET_DATA tr')
+                break
+            except StaleElementReferenceException:
+                LOGGER.info('stale element, trying again')
+                time.sleep(0.5)
+        write_queue.put((bs_table_rows, state_code))
     driver.quit()
 
 
@@ -103,25 +120,30 @@ def writer(target_table_path, work_queue):
 def main():
     thread_list = []
     work_queue = queue.Queue()
+    write_queue = queue.Queue()
+
     for state_code in (
             'AZ', 'AL', 'AK', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI',
             'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI',
             'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC',
             'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
             'VT', 'VA', 'WA', 'WV', 'WI', 'WY'):
+        work_queue.put(state_code)
+
+    for _ in range(N_WORKERS):
         thread_list.append(
             threading.Thread(
-                target=search_state, args=((state_code, work_queue))))
+                target=search_state, args=((work_queue, write_queue))))
         thread_list[-1].start()
-        break
+        work_queue.put('STOP')
 
     write_thread = threading.Thread(
-        target=writer, args=((TABLE_PATH, work_queue)))
+        target=writer, args=((TABLE_PATH, write_queue)))
     write_thread.start()
 
     for search_thread in thread_list:
         search_thread.join()
-    work_queue.put('STOP')
+    write_queue.put('STOP')
     write_thread.join()
 
 
