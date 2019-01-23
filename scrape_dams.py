67532d39948d15ef976a59cc148cc5b0299f20da
Rich Sharp <richpsharp@gmail.com>
2019-01-22 17:54:43 -0800

    threaded queries

--- scrape_dams.py
+++ scrape_dams.py
@@ -1,4 +1,6 @@
 """Scrape dam inventory."""
+import queue
+import threading
 import logging
 import csv
 import os
@@ -16,73 +18,76 @@ LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-def main():
+def search_state(state_code, work_queue):
+    """Search for state `state_code` and dumps result to `work_queue`."""
+    firefox_options = Options()
+    firefox_options.headless = True
+    driver = webdriver.Firefox(options=firefox_options)
+    driver.implicitly_wait(10)
+    driver.get(PRIMARY_URL)
+
+    el = driver.find_element_by_id('P12_ORGANIZATION')
+    for option in el.find_elements_by_tag_name('option'):
+        if option.text == 'Academia':
+            option.click()  # select() in earlier versions of webdriver
+            LOGGER.info('clicked academia')
+            break
+
+    LOGGER.info('click NID Interactive Report')
+    link = driver.find_element_by_link_text('NID Interactive Report')
+    link.click()
+    LOGGER.info('click search')
+    time.sleep(0.1)
+    driver.save_screenshot(f"screenshot_search_{state_code}.png")
+    search_link = driver.find_element_by_id('apexir_SEARCHDROPROOT')
+    search_link.click()
+    time.sleep(0.1)
+    driver.save_screenshot(f"screenshot_searchdropdown_{state_code}.png")
+    el = driver.find_element_by_id('apexir_columnsearch')
+    el.find_element_by_id('STATE').click()
+    LOGGER.info('click STATE')
+    time.sleep(0.1)
+    driver.save_screenshot(f"screenshot_searchstate_{state_code}.png")
+    LOGGER.info(f'type {state_code}')
+    input_box = driver.find_element_by_id('apexir_SEARCH')
+    input_box.clear()
+    input_box.send_keys(state_code)
+    driver.save_screenshot(f"screenshot_searchscode_{state_code}.png")
+    time.sleep(0.1)
+    driver.execute_script("javascript:gReport.search('SEARCH',100)")
+    time.sleep(5.0)
+    driver.save_screenshot(f"screenshot_searcch100_{state_code}.png")
+    LOGGER.info('executed 100000 search')
+    while True:
+        try:
+            table_element = driver.find_element_by_xpath(
+                "//table[@class='apexir_WORKSHEET_DATA']")
+            bs_table_rows = BeautifulSoup(table_element.get_attribute(
+                'outerHTML'), 'html.parser').select(
+                    'table.apexir_WORKSHEET_DATA tr')
+            break
+        except StaleElementReferenceException:
+            LOGGER.info('stale element, trying again')
+            time.sleep(0.5)
+    driver.save_screenshot(f"screenshot_{state_code}.png")
+    work_queue.put((bs_table_rows, state_code))
+    driver.quit()
+
+
+def writer(target_table_path, work_queue):
+    """Writer worker, read `work_queue` for BS tables or 'STOP'."""
     if os.path.exists(TABLE_PATH):
         os.remove(TABLE_PATH)
-
     wrote_header = False
-    for state_code in (
-            'AZ', 'AL', 'AK', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI',
-            'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI',
-            'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC',
-            'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
-            'VT', 'VA', 'WA', 'WV', 'WI', 'WY'):
-
-        firefox_options = Options()
-        firefox_options.headless = True
-        driver = webdriver.Firefox(options=firefox_options)
-        driver.implicitly_wait(10)
-        driver.get(PRIMARY_URL)
-
-        el = driver.find_element_by_id('P12_ORGANIZATION')
-        for option in el.find_elements_by_tag_name('option'):
-            if option.text == 'Academia':
-                option.click()  # select() in earlier versions of webdriver
-                LOGGER.info('clicked academia')
-                break
-
-        LOGGER.info('click NID Interactive Report')
-        link = driver.find_element_by_link_text('NID Interactive Report')
-        link.click()
-        LOGGER.info('click search')
-        time.sleep(0.1)
-        driver.save_screenshot(f"screenshot_search_{state_code}.png")
-        search_link = driver.find_element_by_id('apexir_SEARCHDROPROOT')
-        search_link.click()
-        time.sleep(0.1)
-        driver.save_screenshot(f"screenshot_searchdropdown_{state_code}.png")
-        el = driver.find_element_by_id('apexir_columnsearch')
-        el.find_element_by_id('STATE').click()
-        LOGGER.info('click STATE')
-        time.sleep(0.1)
-        driver.save_screenshot(f"screenshot_searchstate_{state_code}.png")
-        LOGGER.info(f'type {state_code}')
-        input_box = driver.find_element_by_id('apexir_SEARCH')
-        input_box.clear()
-        input_box.send_keys(state_code)
-        driver.save_screenshot(f"screenshot_searchscode_{state_code}.png")
-        time.sleep(0.1)
-        driver.execute_script("javascript:gReport.search('SEARCH',100)")
-        time.sleep(5.0)
-        driver.save_screenshot(f"screenshot_searcch100_{state_code}.png")
-        LOGGER.info('executed 100000 search')
+    with open(target_table_path, 'w', newline='') as table_file:
+        csv_writer = csv.writer(
+            table_file, delimiter=',', quotechar='"',
+            quoting=csv.QUOTE_MINIMAL)
         while True:
-            try:
-                table_element = driver.find_element_by_xpath(
-                    "//table[@class='apexir_WORKSHEET_DATA']")
-                bs_table_rows = BeautifulSoup(table_element.get_attribute(
-                    'outerHTML'), 'html.parser').select(
-                        'table.apexir_WORKSHEET_DATA tr')
+            payload = work_queue.get()
+            if payload == 'STOP':
                 break
-            except StaleElementReferenceException:
-                LOGGER.info('stale element, trying again')
-                time.sleep(0.5)
-        driver.save_screenshot(f"screenshot_{state_code}.png")
-        LOGGER.info("loaded table doc from a string")
-        with open(TABLE_PATH, 'a', newline='') as table_file:
-            csv_writer = csv.writer(
-                table_file, delimiter=',', quotechar='"',
-                quoting=csv.QUOTE_MINIMAL)
+            bs_table_rows, state_code = payload
             for row_index, table_row in enumerate(bs_table_rows):
                 if not wrote_header:
                     csv_writer.writerow(
@@ -93,8 +98,31 @@ def main():
                     LOGGER.info(f'{state_code}, {row_index}')
                 csv_writer.writerow(
                         [col.text for col in table_row.find_all('td')])
-        driver.quit()
+
+
+def main():
+    thread_list = []
+    work_queue = queue.Queue()
+    for state_code in (
+            'AZ', 'AL', 'AK', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI',
+            'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI',
+            'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC',
+            'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
+            'VT', 'VA', 'WA', 'WV', 'WI', 'WY'):
+        thread_list.append(
+            threading.Thread(
+                target=search_state, args=((state_code, work_queue))))
+        thread_list[-1].start()
         break
+
+    write_thread = threading.Thread(
+        target=writer, args=((TABLE_PATH, work_queue)))
+    write_thread.start()
+
+    for search_thread in thread_list:
+        search_thread.join()
+    work_queue.put('STOP')
+    write_thread.join()
 
 
 if __name__ == '__main__':
